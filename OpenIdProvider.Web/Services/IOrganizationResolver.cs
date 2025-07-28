413d6748d32445dae01ad448634d5c676c8d8dd0
Vladyslav <saphire.g8@gmail.com>
2025-07-28 19:50:36 +0200

    Delete old Razor pages project

--- OpenIdProvider.Web/Services/IOrganizationResolver.cs
+++
@@ -1,9 +0,0 @@
-using System.Security.Claims;
-using OpenIdProvider.Data.Models;
-
-namespace OpenIdProvider.Web.Services;
-
-public interface IOrganizationResolver
-{
-    Task<Organization?> ResolvePrimaryOrganizationAsync(ClaimsPrincipal user);
-}
