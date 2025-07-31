04dae34166a35e088cb40d43d11ad2701d066afb
Vladyslav <saphire.g8@gmail.com>
2025-07-31 17:59:50 +0200

    Implement organization-level client permissions - Updated the `OrganizationAddEdit` page to manage Organization-Client permissions via a multi-select UI - Refactored the page to use a dedicated ViewModel

--- OpenIdProvider.Data/Models/UserClientPermission.cs
+++ OpenIdProvider.Data/Models/UserClientPermission.cs
@@ -1,9 +1,8 @@
-
 namespace OpenIdProvider.Data.Models;
 
 public class UserClientPermission
 {
     public Guid UserId { get; set; }
-    public virtual ApplicationUser User { get; set; }  = null!;
+    public virtual ApplicationUser User { get; set; } = null!;
     public int ClientId { get; set; }
 }
